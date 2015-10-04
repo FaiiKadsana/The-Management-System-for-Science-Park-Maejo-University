454a74b458f0bb67e77af7c9f5202c0f628d7487
kamonchatampare <kamonchatsinchai305@gmail.com>
2015-10-04 23:31:53 +0700

    Edit

---
+++ application/views/backend/researcher.php
@@ -0,0 +1,83 @@
+<div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h1 class="page-header">
+                             <small>Home</small>
+                        </h1>
+                        <ol class="breadcrumb">
+                             <li><i class="active"> <i class="fa fa-home"></i></i><a href="<?php echo base_url("backend/index")?>">Home</a></li><li><i class="fa fa-pencil"></i><a href="<?php echo base_url("backend/researcher")?>">จัดการข้อมูลนักวิจัย</a></li>
+                        </ol>
+                    </div>
+                </div></br>
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <center>
+                            <table >
+                              <tr>
+                               <td>
+                               </td>
+                               <td>
+                                  <div class="input-group">
+                                      <input type="text" class="form-control" placeholder="Search ">
+                                      <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
+                                      </span>
+                                  </div>
+                               </td>
+                               <td>
+                               </td>
+                              </tr>
+                             </table>
+                          </center>
+
+                    </div>
+                </div><br>
+                <!-- /.row -->
+                <div class="row">
+                    <div class="col-sm-12">
+                    <div class="panel panel-default">
+          <div class="panel-heading">ข้อมูลนักวิจัย</div>
+            <div class="panel-body">
+               <table class="table table-striped">
+                   <tr class="bg-success"></p>
+                        <td width="15%">#</td>
+                        <td width="20%">รหัสนักวิจัย</td>
+                        <td width="25%">ชื่อ</td>
+                        <td width="25%">นามสกุล</td>                      
+                     </tr>   
+                    
+                    <!-- Show &Join table -->
+
+                     <tr class="bg-success"></p>
+                        <td width="15%">1</td>
+                        <td width="20%">2589</td>
+                        <td width="25%">อลงกรณ์</td>
+                        <td width="25%">อยู่เกิด</td>        
+                        <td><a href=""><button type="button" class="btn btn-info"><i class="fa fa-eye"></i>  ดู</button></a></td>          
+                        <td><a href=""><button type="button" class="btn btn-warning"><i class="fa fa-wrench"></i>  แก้ไข</button></a></td>
+                        <td><a href=""><button type="button" class="btn btn-danger"><i class="fa fa-times"></i>  ลบ</button></a></td>                     
+                     </tr>       
+                     
+                     <!-- End -->
+
+               </table>
+            </div>
+      </div>
+                      
+
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
