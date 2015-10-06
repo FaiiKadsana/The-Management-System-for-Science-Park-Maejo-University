01735bf7a5648ddf21d7a03f50822276ebcc6c9d
kamonchatampare <kamonchatsinchai305@gmail.com>
2015-10-06 16:20:50 +0700

    Edit

--- application/views/backend/researcher.php
+++ application/views/backend/researcher.php
@@ -3,6 +3,7 @@
   <div class="container-fluid">
 
     <!-- Page Heading -->
+    <!-- Star row-->
     <div class="row">
       <div class="col-sm-12">
         <h1 class="page-header">
@@ -14,6 +15,7 @@
      </div>
    </div></br>
    <!-- /.row -->
+   <!-- Star row-->
    <div class="row">
     <div class="col-sm-12">
       <center>
@@ -38,6 +40,7 @@
   </div>
 </div><br>
 <!-- /.row -->
+<!-- Star row-->
 <div class="row">
   <div class="col-sm-12">
     <div class="panel panel-default">
@@ -58,31 +61,184 @@
 
         <!-- Show &Join table -->
 
-        <tr class="bg-success"></p>
+        <tr class="bg-success">
           <td width="15%">1</td>
           <td width="20%">2589</td>
           <td width="25%">อลงกรณ์</td>
           <td width="25%">อยู่เกิด</td>        
           <td><button type="button" class="btn btn-info" data-toggle="modal" data-target=".bs-example-modal-lg"><i class="fa fa-eye"></i>  ดู</button></td> 
-          <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-            <div class="modal-dialog modal-lg">
-              <div class="modal-content">
-                ...
-              </div>
-            </div>
-          </div>
           <td><a href=""><button type="button" class="btn btn-primary"><i class="fa fa-plus"></i> เพิ่ม</button></a></td>          
           <td><a href=""><button type="button" class="btn btn-warning" id="editresearcher"><i class="fa fa-wrench"></i>  แก้ไข</button></a></td>
           <td><a href=""><button type="button" class="btn btn-danger"><i class="fa fa-times"></i>  ลบ</button></a></td>                     
         </tr>       
+      </table>     
+      <!-- End -->
 
-        <!-- End -->
+      <!-- Popup / ดูข้อมูลนัวิจัย-->
+      <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="container-fluid">
 
-      </table>
+              <hr>
+              <!-- Star row-->
+              <div class="row">
+               <!-- รูปนักวิจัย-->
+               <div class="col-sm-2">
+                <img width="145px" height="200px" src="<?php echo base_url("asset2/img/011.jpg") ?>"> <br><br>
+                <center>รหัส : <input type="text" name="idres" disabled value="5268" size="5">  </center>           
+              </div>
 
+              <div class="col-sm-5">
+                <!-- ข้อมูลนักวิจัย -->
 
+                <!-- Show &Join table -->
+                <table class="table table-bordered">
+                  <tr>
+                    <td>ชื่อนักวิจัย : อ. อลงกรณ์  อยู่เกิด</td> 
+                  </tr>
+                  <tr>
+                    <td>Mr. Alongkorn Yukerd</td>
+                  </tr>
+                  <tr>
+                    <td>ตำแหน่ง : อาจารย์</td>
+                  </tr>
+                  <tr>
+                    <td>หน่วยงานสังกัด : clud FriA</td> 
+                  </tr>
+                  <tr>
+                    <td>ที่อยู่ที่สามรถติดต่อไดั : นนทบุรี่</td>
+                  </tr>
+                  <tr>
+                    <td>เบอร์โทรศัยท์ : 0857856130</td>
+                  </tr>
+                  <tr>
+                    <td>E-mail : Alongkornyukerd@gmail.com</td>    
+                  </tr>
+                </table>
+                <!-- End -->
+              </div>
+
+              <div class="col-sm-5">
+                <!-- ความเชี่ยวชาญ -->
+                <div class="panel panel-success">
+                 <div class="panel-heading">ความเชี่ยวชาญ</div>
+
+                 <table class="table table-hover  table-hover table-striped table-condensed">
+                  <tr>
+                    <th>#</th>
+                    <th>ความเชี่ยวชาญ</th>
+                  </tr>
+                  <!-- Show &Join table -->
+                  <tr>
+                    <td>1</td>
+                    <td>คอมพิวเตอร์</td>
+                  </tr>
+                  <!--end -->
+                </table>
+
+              </div>
+            </div>
+
+          </div>
+
+          <!-- /. row-->
+          <!-- star row-->
+          <div class="row">
+            <div class=" col-md-offset-1 col-sm-10">
+             <!-- ผลงานวิจัย -->
+             <div class="panel panel-primary">
+               <div class="panel-heading">ผลงานวิจัย</div>
+
+               <table class="table table-hover  table-hover table-striped table-condensed">
+                <tr>
+                  <th>#</th>
+                  <th>ชื่อผลงานวิจัย</th>
+                  <th style="width: 15%">ปีที่ทำงานวิจัย</th>
+                  <th style="width: 15%">สถาน</th>
+                </tr>
+                <!-- Show &Join table -->
+                <tr>
+                  <td>1</td>
+                  <td>กังหันลบ</td>
+                  <td style="width: 15%">2558</td>
+                  <td style="width: 15%">สำเร็จ</td>
+                </tr>
+                <!--end -->
+              </table>
+
+            </div>
+          </div>
+        </div>
+        <!-- /. row-->
+         <!-- star row-->
+          <div class="row">
+            <div class=" col-md-offset-1 col-sm-10">
+             <!-- ประวัติการทำงาน -->
+             <div class="panel panel-primary">
+               <div class="panel-heading">ประวัติการทำงาน</div>
+
+               <table class="table table-hover  table-hover table-striped table-condensed">
+                <tr>
+                  <th>เริ่มทำงาน</th>
+                  <th>ถึงวันที่</th>
+                  <th>สถานที่ทำงาน</th>
+                  <th>ตำแหน่ง</th>
+                 
+                </tr>
+                <!-- Show &Join table -->
+                <tr>
+                  <td>13/5/2553</td>
+                  <td>ปัจจุบัน</td>
+                  <td>มหาลัยแม่โจ้</td>
+                  <td>อาจารย์</td>
+                   
+                </tr>
+                <!--end -->
+              </table>
+
+            </div>
+          </div>
+        </div>
+        <!-- /. row-->
+        <!-- star row-->
+          <div class="row">
+            <div class=" col-md-offset-1 col-sm-10">
+             <!-- ประวัติการศึกษา -->
+             <div class="panel panel-primary">
+               <div class="panel-heading">ประวัติการศึกษา</div>
+
+               <table class="table table-hover  table-hover table-striped table-condensed">
+                <tr>
+                  <th>ปีการศึกษา</th>
+                  <th>ระดับการศึกษา</th>
+                  <th>คณะ</th>
+                  <th>สาขาวิชา</th>
+                  <th>ชื่อสถาบัน</th>
+                </tr>
+                <!-- Show &Join table -->
+                <tr>
+                  <td>2553</td>
+                  <td>ปริญญาตรี</td>
+                  <td>วิทยาศาสตร์</td>
+                  <td>วิทยาการคอม</td>
+                   <td>แม่โจ้</td>
+                </tr>
+                <!--end -->
+              </table>
+
+            </div>
+          </div>
+        </div>
+        <!-- /. row-->
+        <hr>
+      </div>
     </div>
   </div>
+</div>
+<!--  End Popup / ดูข้อมูลนัวิจัย-->
+</div>
+</div>
 </div>
 </div>
 <!-- /.row -->
--- application/views/backend/script.php
+++ application/views/backend/script.php
@@ -9,7 +9,8 @@
 <script src="<?php echo base_url("asset2/js/plugins/morris/raphael.min.js"); ?>"></script>
 <script src="<?php echo base_url("asset2/js/plugins/morris/morris.min.js"); ?>"></script>
 <script src="<?php echo base_url("asset2/js/plugins/morris/morris-data.js"); ?>"></script>
-
+<script type="text/javascript" src="<?php echo base_url("asset2/dist/js/bootstrapValidator.js"); ?>"></script>
+<link rel="stylesheet" href="<?php echo base_url("asset2/dist/css/bootstrapValidator.css"); ?>"/>
 </body>
 
 </html>
