145d1985e25344e738b416e921d0538472ae71ef
Kadsana Panya <faii_kadsana@hotmail.com>
2015-11-03 20:08:29 +0700

    Science-Park

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -1,3 +1,23 @@
+<style type=”text/css”>
+.calendar {
+font-family: Arial; font-size: 12px;
+}
+table.calendar {
+margin: auto; border-collapse: collapse;
+}
+.calendar .days td {
+width: 60px; height: 60px; padding: 4px;
+border: 1px solid #999;
+vertical-align: top;
+background-color: #DEF;
+}
+.calendar .days td:hover {
+background-color: #FFF;
+}
+.calendar .highlight {
+font-weight: bold; color: #00F;
+}
+</style>
 
 <!-- Welcome Section -->
 <div id="section_header">
@@ -28,6 +48,7 @@
 				<p class='title-header org'>ปฎิทินการนัดหมาย</p>		
 			</center><br>
 			<div class="row">
+<<<<<<< HEAD
 				<div class="col-md-12">
 					<div class="panel panel-default">
 						<div class="panel-heading">
@@ -37,6 +58,20 @@
 							<!-- Calenda -->
 						</div>
 					</div> 
+=======
+				<div class="col-md-4">
+					 <div class="panel panel-default">
+                <div class="panel-heading">
+                  <h2><strong>Calendar</strong></h2>
+                </div><br>
+                <div class="panel-body">
+                  <!-- Calenda -->
+                  <?php echo $calendar; ?>
+                  
+               </div>
+             </div>          
+					
+>>>>>>> Science-Park
 				</div>
 			</div>
 		</div>	 
