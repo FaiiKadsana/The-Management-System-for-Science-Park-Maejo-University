cb82cad24393f55c2642ae1143369cb1dde40841
kamonchatampare <kamonchatsinchat305@gmail.com>
2015-11-03 22:45:48 +0700

    Edit

---
+++ application/views/frontend/tbiplatform.php
@@ -0,0 +1,62 @@
+<!-- Section -->
+<div id="section_header">
+	<h2>TBI Platform</h2>
+</div>
+
+<div class="row">
+	<div class="col-sm-12">
+		<center>
+			<form id="" method="post" class="form-horizontal" action="">
+				<table >
+					<tr>
+						<td><input type="text" class="form-control" placeholder="กรอกเลขแบบฟอร์ม" name="research"></td>
+						<td><button class="btn btn-success" type="button" ><i class="fa fa-search"></i> ค้นหา</button></td>
+						<td><button class="btn btn-success" type="button" >กรอกแบบฟอร์ม</button></td>
+						<td><button class="btn btn-success" type="button" >ดาว์นโหลดแบบฟอร์ม</button></td>
+					</tr>
+				</table>
+			</form>
+		</center>
+
+	</div>
+</div>
+<!-- End  row-->
+<br><br>
+
+<div class="row">
+	<div class="col-md-offset-1  col-sm-10">
+		<div class="panel panel-default">
+			<div class="panel-heading">TBI Platform</div>
+			<div class="panel-body">
+				<table class="table table-striped">
+					<tr class="bg-success">
+						<th width="5%">#</th>
+						<th width="25%">เลขที่แบบฟอร์ม</th>
+						<th width="25%">ชื่อบริษัท</th>
+						<th width="25%">ชื่อโครงการ</th> 
+						<th width="30%">สถานะ</th>     
+						<th></th>
+					</tr>   
+
+					<!-- Show &Join table -->
+
+					<tr class="bg-success">
+						<td >1</td>
+						<td >259863</td>
+						<td >Hotal</td>   
+						<td >กังหันลม</td>
+						<td >อยู่ในช่วงดำเนินงาน</td>
+						<td><a href="<?php echo base_url("backend/")?>"><button type="button" class="btn btn-info" ><i class="fa fa-download"></i>ดาว์นโหลด</button></a></td> 
+						
+					</tr>       
+				</table>     
+				<!-- End -->
+
+
+				<!-- /.row -->
+			</div>
+
+		</div>
+	</div>
+</div>
+       <!-- End  row-->
