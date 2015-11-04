b2abc60b2ee7e084c1682a68c88b994b31aee0dd
Kadsana Panya <faii_kadsana@hotmail.com>
2015-11-04 15:19:44 +0700

    Science-Park

--- application/controllers/frontend/index.php
+++ application/controllers/frontend/index.php
@@ -4,7 +4,8 @@ class Index extends CI_Controller {
 
 	public function __construct (){
 		parent::__construct();
-		//$this->load->model("index_model","index_model");
+		$this->load->library('pagination');
+		
 		//$this->load->library('session');
 
 	}
@@ -13,9 +14,54 @@ class Index extends CI_Controller {
 		$this->load->view('frontend/header');
 		$this->load->view('frontend/menu');
 		$this->load->view('frontend/slider');
-		$this->load->view('frontend/index');
-		$this->load->view('frontend/footer');
+
+		//แบ่งหน้า
+		$config["base_url"]=base_url()."/frontend/index/index";
+		$config["total_rows"] = $this->db->count_all("news");
+		$config["per_page"]=10;
+		$config['uri_segment'] = 4;
+		$config['full_tag_open'] = '<ul>'; 
+		$config['full_tag_close'] = '</ul>'; 
+		$config['num_tag_open'] = '<li>'; 
+		$config['num_tag_close'] = '</li>'; 
+		$config['cur_tag_open'] = '<li class="active"><a><span>'; 
+		$config['cur_tag_close'] = '<span class="sr-only">(current)</span></a></span></li>'; 
+		$config['prev_tag_open'] = '<li>'; 
+		$config['prev_tag_close'] = '</li>'; 
+		$config['next_tag_open'] = '<li>'; 
+		$config['next_tag_close'] = '</li>'; 
+		$config['first_link'] = '&laquo;'; 
+		$config['prev_link'] = '&lsaquo;'; 
+		$config['last_link'] = '&raquo;'; 
+		$config['next_link'] = '&rsaquo;'; 
+		$config['first_tag_open'] = '<li>'; 
+		$config['first_tag_close'] = '</li>'; 
+		$config['last_tag_open'] = '<li>'; 
+		$config['last_tag_close'] = '</li>'; 
+            //จบแบ่งหน้า
+		$this->pagination->initialize($config);
+		
+			//ดึกข้อมูลสำหรับบ่งหน้า
+		$this->db->limit($config['per_page'],$this->uri->segment(4));
+		
+			//*************************
+		$this->db->select('*');
+		$this->db->from('user');
+		$this->db->join('news','user.u_id = news.ne_post');
+		$this->db->join('upload','news.ne_id = upload.up_id_data');
+		$this->db->where('news.ne_type = 1 ');
+		$this->db->group_by("upload.up_id_data"); 
+		$this->db->order_by("ne_date_cre", "desc");
+		$this->db->limit(10,0);
+		$newlist = $this->db->get();
+		$data['index'] = $newlist->result_array();
+
+		$data['page']=$this->pagination->create_links();
+
+		$this->load->view('frontend/index',$data);
 		$this->load->view('frontend/script');	
+		$this->load->view('frontend/footer');
+		
 		
 	}
 
--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -1,24 +1,3 @@
-<style type=”text/css”>
-.calendar {
-font-family: Arial; font-size: 12px;
-}
-table.calendar {
-margin: auto; border-collapse: collapse;
-}
-.calendar .days td {
-width: 60px; height: 60px; padding: 4px;
-border: 1px solid #999;
-vertical-align: top;
-background-color: #DEF;
-}
-.calendar .days td:hover {
-background-color: #FFF;
-}
-.calendar .highlight {
-font-weight: bold; color: #00F;
-}
-</style>
-
 <!-- Welcome Section -->
 <div id="section_header">
 	<h2>ยินดีต้อนรับ</h2>
@@ -32,6 +11,7 @@ font-weight: bold; color: #00F;
 			<center>
 				<p class='title-header org'>ข่าวประชาสัมพันธ์</p>			
 			</center><br>
+<<<<<<< HEAD
 			<div class="row">
 				<div class="col-md-12">
 				<table >
@@ -52,30 +32,63 @@ font-weight: bold; color: #00F;
 							</td>
 						</tr>
 					</table>
-				</div>
-			</div>
-		</div>	
+=======
 
-		<div class= 'col-md-5'>	
-			<center>
-				<p class='title-header org'>ปฎิทินการนัดหมาย</p>		
-			</center><br>
-			<div class="row">
+			<?php 
+			foreach ($newlist as $rownew){
+				?>
 
-				<div class="col-md-12">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							<h2><strong>Calendar</strong></h2>
-						</div><br>
-						<div class="panel-body">
-							<!-- Calenda -->
-							 <?php echo $calendar; ?>
-                 
+				<div class="row">
+					<div class="col-md-4">
+						<div class="col-md-4 " style="heigth:200px;">
+							<?php echo '<img src="'.base_url().$rownew['up_name'].'" title="GETTINGCAMP" " style="height:200px;">'; ?>
 						</div>
-					</div> 
-			</div>
-		</div>	 
+					</div>
 
+					<div class="col-md-8" > 
+						<h5><?php echo anchor("index/news/view/".$rownew['ne_id'],$rownew['ne_sub'],"target='_blank'");?></h5>
+						<?=iconv_substr($rownew['ne_text'],0,350, "UTF-8")."...";?>
+						<div class="row">
+							<div class="col-md-12">
+								<span><i class="fa fa-calendar"></i> <?=date("d - m - Y",strtotime($rownew['ne_date_cre']));?> <i class="fa fa-eye"></i> <?=$rownew['ne_view']?> </span>
+							</div>
+						</div>	
+					</div>
+>>>>>>> Science-Park
+				</div>
+				<?php  } ?>
+
+				<div class="row">
+					<div class="paginationnext" >
+
+						<div class="col-md-12" >
+							<?php echo $page ?>
+						</div>
+					</div>
+				</div>  
+
+			</div>	
+
+			<div class= 'col-md-5'>	
+				<center>
+					<p class='title-header org'>ปฎิทินการนัดหมาย</p>		
+				</center><br>
+				<div class="row">
+
+					<div class="col-md-12">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h2><strong>Calendar</strong></h2>
+							</div><br>
+							<div class="panel-body">
+								<!-- Calenda -->
+								<?php echo $calendar; ?>
+
+							</div>
+						</div> 
+					</div>
+				</div>	 
+			</div>
+		</div>
 	</div>
 </div>
-
