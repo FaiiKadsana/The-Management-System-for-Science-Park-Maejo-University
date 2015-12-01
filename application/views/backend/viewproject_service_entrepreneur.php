392f20712caaa0aa49df9a647c5bf161c2c465f7
kamonchatampare <kamonchatsinchat305@gmail.com>
2015-12-01 16:19:44 +0700

    Edit 1/12/58

---
+++ application/views/backend/viewproject_service_entrepreneur.php
@@ -0,0 +1,121 @@
+ <div id="page-wrapper">
+
+  <div class="container-fluid">
+
+    <!-- Page Heading -->
+    <!-- Star row-->
+    <div class="row">
+      <div class="col-sm-12">
+        <h1 class="page-header">
+         <small>Service Platform</small>
+       </h1>
+       <ol class="breadcrumb">
+         <li class="active"><i class="fa fa-home"></i> <a href="<?php echo base_url("backend/index/display")?>">Home</a></li><li><i class="fa fa-pencil"></i> <a href="<?php echo base_url("backend/project_service")?>">จัดการโครงการ</a></li>
+         <li><i class="fa fa-eye"></i> <a href="<?php echo base_url("frontend/viewproject_service")?>">ดูโครงการ</a></li>
+       </ol>
+     </div>
+   </div></br>
+   <!-- /.row -->
+
+
+        <div class="panel panel-danger">
+          <div class="panel-heading">ข้อมูลผู้ประกอบการ</div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-lg-offset-3  col-sm-6">
+
+                <div class="panel panel-info">
+                  <div class="panel-heading">ข้อมูลผู้ติดต่อ</div>
+
+                  <!-- Show &Join table -->
+
+                  <table class="table table-bordered">
+                    <tr>
+                      <td>ชื่อผู้ติดต่อ : นางสาว กมลฉัตร สินไชย</td> 
+                    </tr>
+                    <tr>
+                      <td> เลขที่บัตรประชาชน: 1929900366417</td>
+                    </tr>
+                    <tr>
+                      <td>ตำแหน่ง: ประชาสัมพันธ์</td>
+                    </tr>
+                    <tr>
+                      <td>เบอร์โทรศัพท์ : 0915247393</td>
+                    </tr>
+                    <tr>
+                      <td>E-mail : kamonchatsinchai305@gmail.com</td>    
+                    </tr>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- End -->
+          <div class="row">
+            <div class="col-lg-offset-3  col-sm-6">
+              <div class="panel panel-info">
+                <div class="panel-heading">ข้อมูลทางธุรกิจ</div>
+
+                <!-- Show &Join table -->
+
+                <table class="table table-bordered">
+                  <tr>
+                    <td>ชื่อกิจการ :  Hotel</td> 
+                  </tr>
+                  <tr>
+                    <td>ชื่อเจ้าของกิจการ : นางสาว เกศนา ปัญญา</td>
+                  </tr>
+                  <tr>
+                    <td>ที่อยู่ : 892/6 ม.3  ตำบล หนองหาร  อำเภอ สันทราย จังหวัด เชียงใหม่</td>
+                  </tr>
+                  <tr>
+                    <td>โทรศัพท์ : 075242500</td> 
+                  </tr>
+                  <tr>
+                    <td>โทรสาร : 075896258</td>
+                  </tr>
+                  <tr>
+                    <td>website : www.Hotlecom.com</td>
+                  </tr>
+                  <tr>
+                    <td>เลขที่ทะเบียนพาณิชย์ : กห28956</td>    
+                  </tr>
+                  <tr>
+                    <td>ประเภทกิจการ: บริษัทจำกัด</td>    
+                  </tr>
+                  <tr>
+                    <td>ลักษณะการประกอบการ : การผลิต</td>    
+                  </tr>
+                  <tr>
+                    <td>สาขาอุตสาหกรรมที่ทำอยู่: ยา</td>    
+                  </tr>
+                  <tr>
+                    <td>เงินจดทะเบียน : 58222 บาท</td>    
+                  </tr>
+                  <tr>
+                    <td>จำนวนปีที่ประกอบการ:2 ปี</td>    
+                  </tr>
+                </table>
+              </div>
+            </div>
+
+            <!-- End -->
+
+          </div>
+        </div> 
+
+        <div class="modal-footer">
+         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+       </div>
+
+     
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
